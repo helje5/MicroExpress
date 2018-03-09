8837642236dc58e406dded501f1bbce7bd477e57
Helge Heß <devteam@zeezide.de>
2018-03-09 18:33:16 +0100

    Bootstrap Xcode package
    Create a new MicroExpress project using swift-xcode.
    
    brew install swiftxcode/swiftxcode/swift-xcode-nio
    swift xcode link-templates
    
    1. Within Xcode, create a new project (Command-Shift-N), and select the
       "Swift-NIO" template.
    2. Give it a name, e.g. "MicroExpress", and make sure that the
         "Include SwiftNIO HTTP1 module"
       option is checked.
    
    Build the project.

---
+++ MicroExpress/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MicroExpress
+//
+//  Created by Helge Hess on 09.03.18.
+//  Copyright © 2018 ZeeZide. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
