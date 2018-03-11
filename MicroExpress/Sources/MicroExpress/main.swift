e9d36d6786666f4f78d39f1c5986db79ca93fb6c
Helge Hess <devteam@zeezide.de>
2018-03-11 17:54:05 +0100

    Add sample routes to main.swift
    A middleware which logs the request and passes on
    control.
    And a middleware which returns Hello World ;-)

--- MicroExpress/Sources/MicroExpress/main.swift
+++ MicroExpress/Sources/MicroExpress/main.swift
@@ -1,4 +1,15 @@
 // File: main.swift - Add to existing file
 let app = Express()
 
+// Logging
+app.use { req, res, next in
+  print("\(req.header.method):", req.header.uri)
+  next() // continue processing
+}
+
+// Request Handling
+app.use { _, res, _ in
+  res.send("Hello, Schwifty world!")
+}
+
 app.listen(1337)
